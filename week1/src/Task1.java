d7b30f9cc74308dc90da22727fd57edc777ed9ec
hcy <1980558787@qq.com>
2021-01-27 17:09:48 +0800

    提交Task1

---
+++ week1/src/Task1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("输入体重(kg):");
+        int weight = in.nextInt();
+
+        System.out.println("输入身高(m):");
+        double height = in.nextDouble();
+
+        double BMI = weight/height/height;
+        System.out.println(BMI);
+        if(BMI<18.5){
+            System.out.println("过轻");
+        }else if(BMI<25){
+            System.out.println("正常");
+        }else if(BMI<28){
+            System.out.println("过重");
+        }else {
+            System.out.println("肥胖");
+        }
+    }
+}
